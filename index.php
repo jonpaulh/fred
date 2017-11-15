a47a149191c982cb943540eb095cd4b75ed6d397
root <root@test-server.(none)>
2017-11-15 16:40:17 +0100

    Getting the hang of it

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 echo "Hello Fred";
 echo "Hello Fred, again";
 echo "Hello Fred, this is going too far";
-echo "Dammit Fred...I think you gone done f'd up';
+echo "Dammit Fred...I think you gone done f'd up"
+echo "Dammit Fred...I think you gone done f'd up twice";
 
 
